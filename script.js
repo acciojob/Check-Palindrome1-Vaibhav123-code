491efd295a116e341ccde98aabf9da89995b86f6
Vaibhav123-code <124595249+Vaibhav123-code@users.noreply.github.com>
2023-04-20 23:09:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // complete the given function
 
 function palindrome(str){
-
-let s1 = str.split(' ').join('')
+let s = str.toLowerCase()
+let s1 = s.split(' ').join('')
 let s2 = s1.split('').reverse().join('');
 
  return true ? s1 == s2 : false;
